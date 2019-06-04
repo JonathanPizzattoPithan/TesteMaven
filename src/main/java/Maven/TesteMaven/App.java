3c9f7b66a680a2a1875fbadf39f8d5d153740ebb
User <User@Dell14>
2019-06-04 13:51:13 -0300

    Teste 01

--- src/main/java/Maven/TesteMaven/App.java
+++ src/main/java/Maven/TesteMaven/App.java
@@ -22,7 +22,7 @@ public class App
  	
     public static void main( String[] args )
     {
-    	System.out.println("Olá");
+    	System.out.println("Olá 2");
         System.out.println( "Dezenas: " + Arrays.toString(getJogo()));
     }
 }
